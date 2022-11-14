c8bacbc64b4c5f34f7806d90a98eecb2eadbe151
khellud <khalidhamid993@gmail.com>
2022-11-14 14:25:07 +0300

    bug fix

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -55,11 +55,13 @@ class _WelcomeScreenState extends State<WelcomeScreen>
           children: <Widget>[
             Row(
               children: <Widget>[
-                Hero(
-                  tag: 'logo',
-                  child: SizedBox(
-                    height: 60,
-                    child: Image.asset('images/logo.png'),
+                Flexible(
+                  child: Hero(
+                    tag: 'logo',
+                    child: SizedBox(
+                      height: 60,
+                      child: Image.asset('images/logo.png'),
+                    ),
                   ),
                 ),
                 AnimatedTextKit(
